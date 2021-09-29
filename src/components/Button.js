7cd1f5e3dfde78bd329fff6fdfbd41900c6ab27c
NehaSijaria <nehasijaria@gmail.com>
2021-09-29 00:52:55 -0400

    build and test button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 import "components/Button.scss";
+import { actions } from "@storybook/addon-actions/dist/preview";
 
 export default function Button(props) {
-   return <></>;
+let buttonClass = "button";
+
+  if (props.confirm) {
+    buttonClass += " button--confirm";
+  } 
+  if (props.danger) {
+     buttonClass += " button--danger";
+  }
+
+  return (
+   <button
+     className={buttonClass}
+     onClick={props.onClick}
+     disabled={props.disabled}
+   >
+     {props.children}
+   </button>
+ );
 }
