50e2e9190058c0a8f01127ded6b620ab7c44b1cc
NehaSijaria <nehasijaria@gmail.com>
2021-09-29 01:15:42 -0400

    add classname and test

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,25 +1,21 @@
 import React from "react";
-
+import classNames from "classnames";
 import "components/Button.scss";
 import { actions } from "@storybook/addon-actions/dist/preview";
 
 export default function Button(props) {
-let buttonClass = "button";
-
-  if (props.confirm) {
-    buttonClass += " button--confirm";
-  } 
-  if (props.danger) {
-     buttonClass += " button--danger";
-  }
-
-  return (
-   <button
-     className={buttonClass}
-     onClick={props.onClick}
-     disabled={props.disabled}
-   >
-     {props.children}
-   </button>
- );
-}
+   const buttonClass = classNames("button", {
+     "button--confirm": props.confirm,
+     "button--danger": props.danger
+   });
+ 
+   return (
+     <button
+       className={buttonClass}
+       onClick={props.onClick}
+       disabled={props.disabled}
+     >
+       {props.children}
+     </button>
+   );
+ }
