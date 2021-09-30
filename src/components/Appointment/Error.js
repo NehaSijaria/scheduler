80983c337f32599392ffac7ad799be90c4cab4b5
NehaSijaria <nehasijaria@gmail.com>
2021-09-30 01:15:18 -0400

    add props to error

--- src/components/Appointment/Error.js
+++ src/components/Appointment/Error.js
@@ -1,16 +1,18 @@
 import React from "react";
 
 export default function Error() {
+  const {message, onClose} = props;
   return (
     <main className="appointment__card appointment__card--error">
       <section className="appointment__error-message">
         <h1 className="text--semi-bold">Error</h1>
-        <h3 className="text--light">Could not delete appointment</h3>
+        <h3 className="text--light">{message}t</h3>
       </section>
       <img
         className="appointment__error-close"
         src="images/close.png"
         alt="Close"
+        onClick ={onClose}
       />
     </main>
   );
