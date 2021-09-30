bb35aa0d0f6db0458c369e1a2310e6531f32b630
NehaSijaria <nehasijaria@gmail.com>
2021-09-29 21:09:14 -0400

    add props to Confirm.js

--- src/components/Appointment/Confirm.js
+++ src/components/Appointment/Confirm.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 export default function Confirm() {
+  const { message, onConfirm, onCancel } = props;
   return (
     <main className="appointment__card appointment__card--confirm">
-      <h1 className="text--semi-bold">Delete the appointment?</h1>
+      <h1 className="text--semi-bold">{message}</h1>
       <section className="appointment__actions">
-        <Button danger>Cancel</Button>
-        <Button danger>Confirm</Button>
+        <Button danger onClick={onCancel}>Cancel</Button>
+        <Button dange onClick={onConfirm}r>Confirm</Button>
       </section>
     </main>
   );
