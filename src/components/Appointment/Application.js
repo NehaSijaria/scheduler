ab59a7084138ade18499582ca29f09b6d1c181a7
NehaSijaria <nehasijaria@gmail.com>
2021-09-30 23:43:33 -0400

    create/add data

---
+++ src/components/Appointment/Application.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+    const appointments = [
+      {
+        id: 1,
+        time: "12pm",
+      },
+      {
+        id: 2,
+        time: "1pm",
+        interview: {
+          student: "Lydia Miller-Jones",
+          interviewer: {
+            id: 1,
+            name: "Sylvia Palmer",
+            avatar: "https://i.imgur.com/LpaY82x.png",
+          }
+        }
+      },
+      {
+        id: 3,
+        time: "3pm",
+        interview: {
+          student: "Archie Cohen",
+          interviewer: {
+            id: 1,
+            name: "Tori Malcolm",
+            avatar: "https://i.imgur.com/LpaY82x.png",
+          }
+        }
+      },
+      {
+        id: 4,
+        time: "4pm",
+        interview: {
+          student: "Maria Boucher",
+          interviewer: {
+            id: 1,
+            name: "Mildred Nazir",
+            avatar: "https://i.imgur.com/LpaY82x.png",
+          }
+        }
+      }
+    ];
+  
+    export default function Application() {
+      return (
+  
+    );
+}
