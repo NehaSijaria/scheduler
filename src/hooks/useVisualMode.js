9a1fc9e6f3a7265eec3e83f88448764da590fe51
NehaSijaria <nehasijaria@gmail.com>
2021-10-05 09:34:49 -0400

    implement back with conditions

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,11 +2,20 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
   function transition(newMode) {
     setMode(newMode);
+    history.push(newMode);
   }
   function back() {
+    if (history.length > 1) {
+      const topItem = history.length - 1;
+      setHistory(history.slice(0, topItem));
+      const previousMode = history[topItem - 1];
+      setMode(previousMode);     
 
+    }
   }
 
   return { mode, transition, back };
