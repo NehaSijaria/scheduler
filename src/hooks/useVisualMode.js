96afd42ae969d6d7201fc186bbb38be7bf9e3452
NehaSijaria <nehasijaria@gmail.com>
2021-10-05 13:16:09 -0400

    add test and implement replacement

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,9 +4,16 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  function transition(newMode) {
-    setMode(newMode);
-    history.push(newMode);
+  function transition(newMode, replace=false) {
+    //if true, then replace by new Item
+    if (replace) {
+      const itemOnTop = history.length - 1;
+      history[itemOnTop] = newMode;
+      setMode(newMode);
+    } else {
+      setMode(newMode);
+      history.push(newMode);
+    }
   }
   function back() {
     if (history.length > 1) {
