78405dfe157b7eaf310fe82db6f52074a0369bd7
NehaSijaria <nehasijaria@gmail.com>
2021-10-07 15:32:22 -0400

    use  best pratices/test.is

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -6,9 +6,13 @@ describe("Navigation", () => {
 
 it("should navigate to Tuesday", () => {
   cy.visit("/");
-  cy.get("li").contains("Tuesday").click();
-  cy.get("li")
-  .contains("li", "Tuesday")
-  .should("have.css", "background-color", "rgb(242, 242, 242)");
+  // cy.get("li").contains("Tuesday").click();
+  // cy.get("li")
+  // .contains("li", "Tuesday")
+  // .should("have.css", "background-color", "rgb(242, 242, 242)");
+  //refactor above two..
+  cy.contains("[data-testid=day]", "Tuesday")
+  .click()
+  .should("have.class", "day-list__item--selected");
 });
 
