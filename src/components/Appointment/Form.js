3529884b3da0da74e4a85de631b55976bc58d1fa
NehaSijaria <nehasijaria@gmail.com>
2021-10-07 00:25:53 -0400

    create form test

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -4,9 +4,10 @@ import { useState } from 'react';
 import Button from 'components/Button'
 
 export default function Form(props  ) {
-  const {interviewers, onSave, onCancel } = props;
+  const {interviewers, onCancel } = props;
   const [name, setName] = useState(props.name || ""); 
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   //Helper function to clear form values
 
@@ -20,21 +21,35 @@ export default function Form(props  ) {
     onCancel();
   }
 
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  
+    props.onSave(name, interviewer);
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
   <section className="appointment__card-left">
     <form autoComplete="off" onSubmit={event => event.preventDefault()}>
       <input
-        onChange={event => setName(event.target.value)}
+        onChange={event => {
+          setName(event.target.value);
+        }}
         className="appointment__create-input text--semi-bold"
         value={name}
         type="text"
         placeholder="Enter Student Name"
+        name="name"
         /*
           This must be a controlled component
         */
+        data-testid="student-name-input"
       />
     </form>
+    <section className="appointment__validation">{error}</section>
     <InterviewerList 
       interviewers={props.interviewers} 
       value={interviewer}   
@@ -46,7 +61,8 @@ export default function Form(props  ) {
       <Button danger onClick={onCancel}>
         Cancel
       </Button>
-      <Button confirm onClick={event => props.onSave(name, interviewer)}>
+      {/* <Button confirm onClick={event => props.onSave(name, interviewer)}> */}
+      <Button confirm onClick={validate}> 
         Save
       </Button>
     </section>
