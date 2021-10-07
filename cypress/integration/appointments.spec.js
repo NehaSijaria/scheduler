04c4dc5e9a647add07f9ef2bf9fc809492761d0e
NehaSijaria <nehasijaria@gmail.com>
2021-10-07 16:46:11 -0400

    chhose interviewer

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,11 +1,11 @@
 describe("Appointments", () => {
   it("should book an interview", () => {
     cy.visit("/");
-    cy.contains("Monday");
+    cy.contains("Tuesday"); 
     cy.get("[alt=Add]")
     .first()
     .click();
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
-  });  
- 
-})
+    cy.get("[alt='Sylvia Palmer']").click();
+  });
+});
